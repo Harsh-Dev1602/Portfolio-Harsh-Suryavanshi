5dfd5d78f8a0a6fbdc180e863adb48dee0d4150d
Harsh Suryavanshi <harshsuryavanshi16022003@gmail.com>
2025-02-20 00:56:45 +0530

    portfolio-aboutus-update

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -9,13 +9,9 @@ function AboutUs() {
     <p className=' text-xl mb-5 '>Throughout my academic journey, I have had the opportunity to work on various hands-on projects, including a To-Do List application and a QR Code Generator, as well as collaborating with peers on a Comprehensive Notes project as part of a minor. These experiences have not only helped me refine my technical skills but have also allowed me to grow as a problem-solver and communicator.</p>
     <p className=' text-xl mb-5 '>I am particularly interested in User Experience Design and website development, and I am eager to apply my knowledge and skills in real-world settings. This portfolio showcases some of my best work to date, reflecting my growth, creativity, and dedication to my craft.</p>
 
-    <p className=' text-xl mb-5 '>I am excited to continue building on these experiences and look forward to the future. Thank you for exploring my journey so far!Hello! I’m Harsh Suryavanshi, a passionate and dedicated third-year student currently pursuing a Information Technology at Acropolis Institute of Technology & Research. With a strong foundation in HTML, CSS, JS, Full-stack development & MERN stack, I am constantly learning and evolving as I approach the final stages of my degree.</p>
+    <p className=' text-xl mb-5 '>I am excited to continue building on these experiences and look forward to the future. Thank you for exploring my journey so far!</p>
 
-    <p className=' text-xl mb-5 '>Throughout my academic journey, I’ve had the opportunity to work on various hands-on projects, from To-Do List & QR-Generator to collaborating with peers on Comprehensive Notes in a Minor project. These experiences have helped me not only refine my technical skills but also grow as a problem-solver and communicator.</p>
 
-    <p className=' text-xl mb-5 '>I'm particularly interested in User Experience Design & website development, and I am eager to apply my knowledge and skills in real-world settings. This portfolio showcases some of my best work to date, reflecting my growth, creativity, and dedication to my craft.</p>
-
-    <p className=' text-xl  '>I’m excited to continue building on these experiences and look forward to the future. Thank you for exploring my journey so far!</p>
     </main>
     </>
   )
