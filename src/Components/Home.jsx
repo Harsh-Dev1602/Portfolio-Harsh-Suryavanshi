9a35849a77d3f4a5d89f1097c00a56b860325324
Harsh Suryavanshi <harshsuryavanshi16022003@gmail.com>
2025-02-27 22:04:44 +0530

    up-to-date-2025.0.0.1.5

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { FaSquareGithub } from "react-icons/fa6";
 import { FaLinkedin } from "react-icons/fa";
-import Photo from "../../public/Img/photo.png";
+import Photo from "../../public/Img/Photo.jpg";
 
 function Home() {
    const Icon = [
