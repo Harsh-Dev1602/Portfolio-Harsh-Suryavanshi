59c2664e3fbb79e3ece98632e46fa5a8732ae45a
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-04-18 23:56:11 +0530

    up-to-date-2025.0.0.1.8

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Img1 from "../../public/Img/Project1.png"
 import Img2 from "../../public/Img/Project2.png"
 import Img3 from "../../public/Img/Project3.png"
+import Img4 from "../../public/Img/Project4.png"
+
 
 
 function Project() {
@@ -17,7 +19,17 @@ function Project() {
               <h3 className="text-2xl font-bold">Comprehensive Notes</h3>
               <p className="mt-2 text-justify">The Comprehensive Digital Learning System for Students provides an intuitive and user-friendly platform for accessing, sharing, and engaging with study materials.
 
-                <a href="https://comprehensive-notes-minor-project.vercel.app/SignUpLogIn.html " className='text-blue-700 hover:underline font-bold  p-1.5 ' target="_blank" rel="noopener noreferrer">Visit My Project</a>
+                <a href="https://comprehensive-notes.vercel.app" className='text-blue-700 hover:underline font-bold  p-1.5 ' target="_blank" rel="noopener noreferrer">Visit My Project</a>
+              </p>
+
+            </div>
+            <div className="p-6 bg-white  shadow shadow-gray-500 rounded-2xl ">
+              <img src={Img4} />
+              <h3 className="text-2xl font-bold">Chat-Application
+              </h3>
+              <p className="mt-2 text-justify">A Chat Application is a real-time communication platform that allows users to send and receive text, and multimedia messages over the internet. chat apps support features such as message notifications  and status updates.
+
+                <a href="https://chat-application-a5mv.onrender.com" className='text-blue-700 hover:underline font-bold  p-1.5 ' target="_blank" rel="noopener noreferrer">Visit My Project</a>
               </p>
 
             </div>
@@ -25,10 +37,9 @@ function Project() {
               <img src={Img2} />
               <h3 className="text-2xl font-bold">To-Do List</h3>
 
-              <p className="mt-2 text-justify">
-                <li className=' list-none'><span className=' font-semibold'>Organized:</span> It helps you keep your tasks in a structured manner.</li>
-                <li className=' list-none'><span className=' font-semibold'>Prioritized:</span> You can arrange tasks based on their importance or urgency</li>
-                <li className=' list-none'><span className=' font-semibold'>Comprehensive:</span> It captures all the tasks you need to complete<a href="https://to-do-list-gamma-nine-10.vercel.app" className='text-blue-700 font-bold hover:underline  p-1.5 ' target="_blank" rel="noopener noreferrer">Visit My Project</a></li>
+              <p className="mt-2 text-justify">  
+                A To-Do List is a tool used to organize tasks that need to be completed. As tasks are completed, they can be marked off or removed from the list.
+                  <a href="https://to-do-list-gamma-nine-10.vercel.app" className='text-blue-700 font-bold hover:underline  p-1.5 ' target="_blank" rel="noopener noreferrer">Visit My Project</a>
               </p>
 
             </div>
@@ -36,9 +47,8 @@ function Project() {
               <img src={Img3} />
               <h3 className="text-2xl font-bold">QR-Generator</h3>
               <p className="mt-2 text-justify">
-                <li className=" list-none"><span className=' font-semibold'>Innovative: </span>It uses modern technology to create QR codes.</li>
-                <li className=" list-none"><span className=' font-semibold'>Convenient:</span> It simplifies the process of sharing information.</li>
-                <li className=" list-none"><span className=' font-semibold'>Efficient:</span> It quickly generates QR codes for various uses. <a href="https://qr-generator-three-wheat.vercel.app" className='text-blue-700 font-bold  hover:underline  p-1.5 ' target="_blank" rel="noopener noreferrer ">Visit My Project</a></li> 
+              A QR Generator is a tool that creates QR codes (Quick Response codes), which are two-dimensional barcodes that can store information such as text, URLs, contact details, or other data. 
+                 <a href="https://qr-generator-three-wheat.vercel.app" className='text-blue-700 font-bold  hover:underline  p-1.5 ' target="_blank" rel="noopener noreferrer ">Visit My Project</a>
 
               </p>
 
