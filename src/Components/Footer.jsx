86f0fd0edab699b7a4b274657ab9023c45ccce53
Harsh Suryavanshi <Harsh-Dev1602@users.noreply.github.com>
2025-06-25 14:07:28 +0530

    up-to-date

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -18,19 +18,21 @@ function Footer() {
   ];
   return (
     <>
-      <footer className='w-full h-auto rounded-t-2xl bg-black flex flex-col lg:flex-row py-20 gap-5 px-0 md:px-20 justify-between items-center  '>
+      <footer className='w-full h-auto rounded-t-2xl bg-black flex flex-col md:flex-row py-20 gap-5 px-0 md:px-20 justify-between items-center  '>
         <div className=" order-2 text-center  md:order-1 text-white text-xl flex  gap-2 md:text-2xl">
-        <h1>© 2025,</h1>
-        <span> All rights reserved.</span>
+          <h1>© 2025,</h1>
+          <span> All rights reserved.</span>
         </div>
-        <ul className=' order-1 md:order-2 flex items-center justify-center gap-4 shadow shadow-[#ccc] rounded-2xl py-1 px-2.5'>
-
-          {
-            Icon.map(({ id, icon, link }) => (
-              <a key={id} href={link} className=' p-2 rounded-md text-white hover:bg-[#cccccc17] text-3xl cursor-pointer' target="_blank" rel="noopener noreferrer">{icon}</a>
-            ))
-          }
-        </ul>
+        <div className=' order-1 md:order-2 flex items-center justify-center gap-4 shadow flex-col shadow-[#ccc] rounded-2xl py-1 px-10'>
+          <h1 className=' text-white'>FOLLOW US</h1>
+          <ul className='flex'>
+            {
+              Icon.map(({ id, icon, link }) => (
+                <a key={id} href={link} className=' p-2 rounded-md text-white hover:bg-[#cccccc17] text-3xl cursor-pointer' target="_blank" rel="noopener noreferrer">{icon}</a>
+              ))
+            }
+          </ul>
+        </div>
       </footer>
     </>
   )
